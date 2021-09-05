684536372f8091961186d773202afc7417894ed8
LamKamhang <lamkamh@zju.edu.cn>
2021-09-05 16:25:33 +0800

    update utility

--- utility.cmake
+++ utility.cmake
@@ -6,10 +6,10 @@ include(CMakeParseArguments)
 # LIBS are the extra libs needed to compile the target.
 function(add_unit name)
   # cmake_parse_arguments(<prefix> <options> <one_value_keywords> <multi_value_keywords> args...)
-  cmake_parse_arguments("arg" "" "" "SRC;DEFS;FTRS;LIBS" ${ARGN})
+  cmake_parse_arguments("arg" "" "" "SRCS;DEFS;FTRS;LIBS" ${ARGN})
 
-  if(DEFINED arg_SRC)
-    set(source ${arg_SRC})
+  if(DEFINED arg_SRCS)
+    set(source ${arg_SRCS})
   else()
     set(source ${name}.cc)
   endif()
@@ -48,6 +48,15 @@ endfunction()
 #   )
 # or
 # add_git_3rd_project(eigen "ssh://git@ryon.ren:10022/mirrors/eigen3.git" "3.3.9")
+#
+# if the project does not have a CMakeLists or you do not want to build the 3rd project,
+# you should pass DO_NOT_BUILD flag.
+# Example:
+# add_3rd_project(igl
+#   GIT_REPOSITORY "ssh://git@ryon.ren:10022/mirrors/libigl.git"
+#   GIT_TAG        "v2.2.0"
+#   DO_NOT_BUILD
+#   )
 function(add_3rd_project name)
   set(oneValueArgs
     PREFIX
@@ -55,7 +64,8 @@ function(add_3rd_project name)
     INSTALL_DIR
     )
   set(multiValueArgs "")
-  cmake_parse_arguments("EXT_PROJ" "" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+  set(options DO_NOT_BUILD)
+  cmake_parse_arguments("EXT_PROJ" "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
   message(STATUS "Downloading/updating ${name}")
   set(EXT_PROJ_NAME ${name})
   set(EXT_PROJ_TMP_DIR ${CMAKE_BINARY_DIR}/tmp)
@@ -72,6 +82,14 @@ function(add_3rd_project name)
   endif()
   if (NOT EXT_PROJ_INSTALL_DIR)
     set(EXT_PROJ_INSTALL_DIR ${EXT_PROJ_PREFIX})
+  endif()
+  if (EXT_PROJ_DO_NOT_BUILD)
+    set(EXT_PROJ_UNPARSED_ARGUMENTS ${EXT_PROJ_UNPARSED_ARGUMENTS}
+      CONFIGURE_COMMAND "${CMAKE_COMMAND} -E echo do nothing in configure step"
+      BUILD_COMMAND     "${CMAKE_COMMAND} -E echo do nothing in build step"
+      TEST_COMMAND      "${CMAKE_COMMAND} -E echo do nothing in test step"
+      INSTALL_COMMAND   "${CMAKE_COMMAND} -E echo do nothing in install step"
+      )
   endif()
   configure_file("${UTILITY_DIR}/extproj.cmake.in"
     "${EXT_PROJ_CACHE_DIR}/CMakeLists.txt")
@@ -113,8 +131,8 @@ function(add_url_3rd_project name url)
     )
 endfunction()
 
-# downloaded dependencies.
-macro(require_package pkg url tag)
+# download dependencies.
+macro(require_git_package pkg url tag)
   set(pkg_FOUND ${pkg}_FOUND)
   set(pkg_VERSION ${pkg}_VERSION)
   find_package(${pkg})
@@ -126,38 +144,13 @@ macro(require_package pkg url tag)
   message("Find ${pkg} version -> ${${pkg_VERSION}}")
 endmacro()
 
-function(fetch_git_3rd_project name url tag)
-  cmake_parse_arguments("" "" "" "" ${ARGN})
-  add_3rd_project(${name}
-    GIT_REPOSITORY ${url}
-    GIT_TAG ${tag}
-    ${ARGN}
-    CONFIGURE_COMMAND "${CMAKE_COMMAND} -E echo do nothing in configure step"
-    BUILD_COMMAND  "${CMAKE_COMMAND} -E echo do nothing in build step"
-    TEST_COMMAND  "${CMAKE_COMMAND} -E echo do nothing in test step"
-    INSTALL_COMMAND  "${CMAKE_COMMAND} -E echo do nothing in install step"
-    )
-endfunction()
-
-function(fetch_url_3rd_project name url)
-  cmake_parse_arguments("" "" "" "" ${ARGN})
-  add_3rd_project(${name}
-    URL ${url}
-    ${ARGN}
-    CONFIGURE_COMMAND "${CMAKE_COMMAND} -E echo do nothing in configure step"
-    BUILD_COMMAND  "${CMAKE_COMMAND} -E echo do nothing in build step"
-    TEST_COMMAND  "${CMAKE_COMMAND} -E echo do nothing in test step"
-    INSTALL_COMMAND  "${CMAKE_COMMAND} -E echo do nothing in install step"
-    )
-endfunction()
-
-macro(fetch_package pkg url tag)
+macro(require_url_package pkg url)
   set(pkg_FOUND ${pkg}_FOUND)
   set(pkg_VERSION ${pkg}_VERSION)
   find_package(${pkg})
   if (NOT ${pkg_FOUND})
     message("Ready to download ${pkg}...")
-    fetch_git_3rd_project(${pkg} ${url} ${tag} ${ARGN})
+    add_url_3rd_project(${pkg} ${url} ${ARGN})
     find_package(${pkg} REQUIRED)
   endif()
   message("Find ${pkg} version -> ${${pkg_VERSION}}")
