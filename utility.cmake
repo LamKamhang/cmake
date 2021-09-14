ff67156d6ea0813abedd76da268cec8fe4561cdc
LamKamhang <lamkamh@zju.edu.cn>
2021-09-14 22:50:29 +0800

    bugfix: symlink failed on windows

--- utility.cmake
+++ utility.cmake
@@ -31,11 +31,15 @@ function(add_unit name)
   endif()
 endfunction()
 
-function(add_symlink name src dest)
-  add_custom_target(${name} ALL
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${src} ${dest}
-    COMMENT "mklink ${src} -> ${dest}"
-    )
+function(add_symlink src dest)
+  message(STATUS "mklink ${src} -> ${dest}")
+  file(TO_NATIVE_PATH "${dest}" _dstDir)
+  file(TO_NATIVE_PATH "${src}" _srcDir)
+  if (WIN32)
+      execute_process(COMMAND cmd.exe /c mklink /J "${_dstDir}" "${_srcDir}")
+  else()
+      execute_process(COMMAND "${CMAKE_COMMAND}" -E create_symlink "${_srcDir}" "${_dstDir}")
+  endif()
 endfunction()
 
 # Usage: add_3rd_project(project_name
