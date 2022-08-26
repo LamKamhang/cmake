25f66d7354055af13c37f899f5a9474e163aaad6
LamKamhang <lamkamh@zju.edu.cn>
2022-08-26 15:33:33 +0800

    update target helper

--- core/cmake-core-target-helper.cmake
+++ core/cmake-core-target-helper.cmake
@@ -1,16 +1,16 @@
-# Usage: add_unit(target_name
-#                 [SRC          <file.cpp/file.cc>]
+# Usage: add_unit(target_name [isLIB] [INTERFACE]
+#                 [SRCS         <file.cpp/file.cc>]
 #                 [LIBS         <libs/other_targets>]
 #                 [INCLUDE_PATH <include_paths>]
 #                 [LINK_PATH    <link_paths>]
 #                 [DEFS         <defs>]
-#                 [FTRS         <features>]
+#                 [FEATS        <features>]
 #                 )
 # If SRC is not specfied, ${target_name}.cc is used as the default source.
 function(add_unit name)
   # cmake_parse_arguments(<prefix> <options> <one_value_keywords> <multi_value_keywords> args...)
-  set(options isLIB)
-  set(multiValueArgs "SRCS;LIBS;INCLUDE_PATH;LINK_PATH;DEFS;FTRS")
+  set(options isLIB INTERFACE)
+  set(multiValueArgs "SRCS;LIBS;INCLUDE_PATH;LINK_PATH;DEFS;FEATS")
   cmake_parse_arguments(
     arg "${options}" "" "${multiValueArgs}" ${ARGN}
     )
@@ -19,29 +19,35 @@ function(add_unit name)
     set(arg_SRCS ${name}.cc)
   endif()
 
-  if (arg_isLIB)
-    add_library(${name} ${arg_SRCS})
+  if (arg_INTERFACE)
+    set(scope INTERFACE)
   else()
-    add_executable(${name} ${arg_SRCS})
+    unset(scope)
+  endif()
+
+  if (arg_isLIB)
+    add_library(${name} ${scope} ${arg_SRCS})
+  else()
+    add_executable(${name} ${scope} ${arg_SRCS})
   endif()
 
   if (DEFINED arg_LIBS)
-    target_link_libraries(${name} ${arg_LIBS})
+    target_link_libraries(${name} ${scope} ${arg_LIBS})
   endif()
 
   if (DEFINED arg_INCLUDE_PATH)
-    target_include_directories(${name} ${arg_INCLUDE_PATH})
+    target_include_directories(${name} ${scope} ${arg_INCLUDE_PATH})
   endif()
 
   if (DEFINED arg_LINK_PATH)
-    target_link_directories(${name} ${arg_LINK_PATH})
+    target_link_directories(${name} ${scope} ${arg_LINK_PATH})
   endif()
 
   if (DEFINED arg_DEFS)
-    target_compile_definitions(${name} ${arg_DEFS})
+    target_compile_definitions(${name} ${scope} ${arg_DEFS})
   endif()
 
-  if (DEFINED arg_FTRS)
-    target_compile_features(${name} ${arg_FTRS})
+  if (DEFINED arg_FEATS)
+    target_compile_features(${name} ${scope} ${arg_FEATS})
   endif()
 endfunction()
