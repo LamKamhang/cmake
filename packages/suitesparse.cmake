aea47704c8c544ef8b7e64fa0fbe7bd8f50c9645
LamKamhang <lamkamh@zju.edu.cn>
2022-12-17 19:14:47 +0800

    suitesparse should enable C

--- packages/suitesparse.cmake
+++ packages/suitesparse.cmake
@@ -2,6 +2,7 @@ include_guard()
 
 message(STATUS "[package/SuiteSparse]: SuiteSparse::AMD SuiteSparse::BTF SuiteSparse::CAMD SuiteSparse::CCOLAMD SuiteSparse::COLAMD SuiteSparse::UMFPACK SuiteSparse::KLU SuiteSparse::LDL SuiteSparse::Config SuiteSparse::CHOLMOD SuiteSparse::Partition SuiteSparse::SPQR SuiteSparse::gpuruntime SuiteSparse::gpuqrengine")
 
+enable_language(C)
 require_package(SuiteSparse "gh:sergiud/SuiteSparse.git#5.13.0-cmake.3"
   CMAKE_ARGS "-DBUILD_SHARED_LIBS=ON"
   CMAKE_ARGS "-DWITH_DEMOS=OFF")
