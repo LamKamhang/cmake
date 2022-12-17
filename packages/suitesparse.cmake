c2aa7bf11ba237294ed6e8d91319f30d52ccbbcf
LamKamhang <lamkamh@zju.edu.cn>
2022-12-17 22:32:22 +0800

    update package

--- packages/suitesparse.cmake
+++ packages/suitesparse.cmake
@@ -2,8 +2,16 @@ include_guard()
 
 message(STATUS "[package/SuiteSparse]: SuiteSparse::AMD SuiteSparse::BTF SuiteSparse::CAMD SuiteSparse::CCOLAMD SuiteSparse::COLAMD SuiteSparse::UMFPACK SuiteSparse::KLU SuiteSparse::LDL SuiteSparse::Config SuiteSparse::CHOLMOD SuiteSparse::Partition SuiteSparse::SPQR SuiteSparse::gpuruntime SuiteSparse::gpuqrengine")
 
+option(SUITESPARSE_IMPORT_AS_SUBDIR "Import as subdirectory instead of install && find_package" OFF)
+
 enable_language(C)
-require_package(SuiteSparse "gh:sergiud/SuiteSparse.git#5.13.0-cmake.3"
-  GIT_PATCH  "${CMAKE_UTILITY_PATCH_DIR}/suitesparse.cholmod.patch"
-  CMAKE_ARGS "-DBUILD_SHARED_LIBS=ON"
-  CMAKE_ARGS "-DWITH_DEMOS=OFF")
+
+if (SUITESPARSE_IMPORT_AS_SUBDIR)
+  require_package(SuiteSparse "gh:sergiud/SuiteSparse.git#5.13.0-cmake.3"
+    CMAKE_ARGS "-DWITH_DEMOS=OFF")
+else()
+  require_package(SuiteSparse "gh:sergiud/SuiteSparse.git#5.13.0-cmake.3"
+    SUBDIR_ONLY
+    SUB_BINARY "${CMAKE_BINARY_DIR}/3rd/suitesparse"
+    CMAKE_ARGS "-DWITH_DEMOS=OFF")
+endif()
