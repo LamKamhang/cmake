0b9afe6211fbca95c6d044ec361d4b0297287c08
LamKamhang <lamkamh@zju.edu.cn>
2023-04-13 18:53:02 +0800

    fix nanoflann

--- packages/pkg_nanoflann.cmake
+++ packages/pkg_nanoflann.cmake
@@ -12,5 +12,5 @@ set(nanoflann_VERSION 1.4.3 CACHE STRING "nanoflann customized version")
 require_package(nanoflann "gh:jlblancoc/nanoflann#v${nanoflann_VERSION}"
   CMAKE_ARGS "-DNANOFLANN_BUILD_TESTS=OFF"
   CMAKE_ARGS "-DNANOFLANN_BUILD_BENCHMARKS=OFF"
-  CMAKE_ARGS "-DNANOFLANN_BUILD_TESTS=OFF"
+  CMAKE_ARGS "-DNANOFLANN_BUILD_EXAMPLES=OFF"
 )
