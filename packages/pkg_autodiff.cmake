b52eeaaee5c990f6ab282afefc963d084f3caa90
LamKamhang <lamkamh@zju.edu.cn>
2023-04-14 21:53:41 +0800

    fix autodiff for float

--- packages/pkg_autodiff.cmake
+++ packages/pkg_autodiff.cmake
@@ -7,9 +7,10 @@ endif()
 
 message(STATUS "[package/autodiff]: autodiff::autodiff")
 
-set(autodiff_VERSION 0.6.12 CACHE STRING "autodiff customized version")
+set(autodiff_VERSION 1.0.1 CACHE STRING "autodiff customized version")
 
 require_package(autodiff "gh:autodiff/autodiff.git#v${autodiff_VERSION}"
+  GIT_PATCH "${CMAKE_UTILITY_PATCH_DIR}/autodiff.patch"
   CMAKE_ARGS "-DAUTODIFF_BUILD_TESTS=OFF"
   CMAKE_ARGS "-DAUTODIFF_BUILD_PYTHON=OFF"
   CMAKE_ARGS "-DAUTODIFF_BUILD_EXAMPLES=OFF"
