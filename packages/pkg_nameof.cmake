c7477cfda5ebac739b94549c89d91a53b24a07f3
LamKamhang <lamkamh@zju.edu.cn>
2023-04-18 20:18:37 +0800

    fixtypo

--- packages/pkg_nameof.cmake
+++ packages/pkg_nameof.cmake
@@ -1,4 +1,4 @@
-include_gurad()
+include_guard()
 
 # if nameof::nameof has been found
 if (TARGET nameof::nameof)
@@ -9,7 +9,7 @@ message(STATUS "[package/nameof]: nameof::nameof")
 
 set(nameof_VERSION 0.10.3 CACHE STRING "nameof customized version")
 
-require_package(nameof "gh:Neargye/nameof.git#{nameof_VERSION}"
+require_package(nameof "gh:Neargye/nameof#v${nameof_VERSION}"
   CMAKE_ARGS "-DNAMEOF_OPT_BUILD_EXAMPLES=OFF"
   CMAKE_ARGS "-DNAMEOF_OPT_BUILD_TESTS=OFF"
   CMAKE_ARGS "-DNAMEOF_OPT_INSTALL=OFF"
