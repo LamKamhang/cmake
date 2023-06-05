772910aebef8fb9172ef6d85b14f0a6ca827a4b0
LamKamhang <lamkamh@zju.edu.cn>
2023-06-05 20:41:12 +0800

    add some function

--- core/cmake-core-target-helper.cmake
+++ core/cmake-core-target-helper.cmake
@@ -12,14 +12,17 @@ include_guard()
 # If SRC is not specfied, ${target_name}.cc/cpp is used as the default source.
 function(add_unit name)
   # cmake_parse_arguments(<prefix> <options> <one_value_keywords> <multi_value_keywords> args...)
-  set(options isLIB INTERFACE)
+  set(options isLIB INTERFACE APPEND_SOURCE)
   set(oneValueArgs "ALIAS")
   set(multiValueArgs "GLOB_SRCS;SRCS;LIBS;INCLUDE_DIRS;LINK_DIRS;DEFS;FEATS;OPTIONS")
   cmake_parse_arguments(
     arg "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN}
   )
 
-  if (TARGET name)
+  if (arg_APPEND_SOURCE AND NOT TARGET ${name})
+    message(FATAL_ERROR "Append Source to Target(${name}) failed, since ${name} isn't created.")
+  endif()
+  if (NOT arg_APPEND_SOURCE AND TARGET ${name})
     message(FATAL_ERROR "Target: ${name} has been created.")
   endif()
 
@@ -51,7 +54,9 @@ function(add_unit name)
   endif()
 
   # create target.
-  if (arg_isLIB)
+  if (arg_APPEND_SOURCE)
+    target_sources(${name} PRIVATE ${arg_SRCS})
+  elseif (arg_isLIB)
     add_library(${name} ${arg_SRCS})
   elseif (arg_INTERFACE)
     add_library(${name} INTERFACE ${arg_SRCS})
@@ -109,5 +114,89 @@ function(add_unit name)
 
   if (DEFINED arg_ALIAS)
     add_library(${arg_ALIAS} ALIAS ${name})
+  endif()
+endfunction()
+
+# add_benchmark
+function(chaos_add_benchmark target_name)
+  if (NOT TARGET benchmark::benchmark_main)
+    declare_pkg_deps(benchmark)
+  endif()
+  add_unit(${ARGV} LIBS benchmark::benchmark_main)
+  set_target_properties(${target_name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/benchmark")
+endfunction()
+
+function(chaos_add_example name)
+  add_unit(${ARGV})
+  set_target_properties(${name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/examples")
+endfunction()
+
+function(chaos_add_app name)
+  add_unit(${ARGV})
+  set_target_properties(${name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/apps")
+endfunction()
+
+function(chaos_add_test name)
+  add_unit(${ARGV})
+  set_target_properties(${name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/tests")
+endfunction()
+
+function(chaos_add_unittest name)
+  if (NOT TARGET Catch2::Catch2)
+    declare_pkg_deps(Catch2)
+  endif()
+
+  set(options CUSTOM_MAIN DISABLE_DEFAULT_GLOB)
+  set(oneValueArgs "")
+  set(multiValueArgs "GLOB_SRCS;SRCS")
+  cmake_parse_arguments(OPTIONS "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
+
+  if (NOT OPTIONS_DISABLE_GLOB)
+    set(UNITTEST_GLOB_PATTERN
+      "unittest/*.cc" "unittest/*.hh" "unittest/*.[ch]pp" "unittest/*.[ch]xx"
+    )
+  endif()
+
+  if (DEFINED OPTIONS_GLOB_SRCS)
+    list(APPEND UNITTEST_GLOB_PATTERN ${OPTIONS_GLOB_SRCS})
+  endif()
+  file(GLOB_RECURSE UNIT_SRCS ${UNITTEST_GLOB_PATTERN})
+  if (DEFINED OPTIONS_SRCS)
+    list(APPEND UNIT_SRCS ${OPTIONS_SRCS})
+  endif()
+
+  list(LENGTH UNIT_SRCS NUM_OF_FILES)
+  if (${NUM_OF_FILES} EQUAL 0)
+    message(STATUS "skip ${name}, cannot find any source file.")
+    return()
+  endif()
+
+  if (TARGET ${name})
+      add_unit(${name}
+        APPEND_SOURCE
+        SRCS ${UNIT_SRCS}
+        ${OPTIONS_UNPARSED_ARGUMENTS}
+      )
+  else()
+    add_unit(${name}
+      SRCS ${UNIT_SRCS}
+      ${OPTIONS_UNPARSED_ARGUMENTS}
+    )
+
+    if (NOT OPTIONS_CUSTOM_MAIN)
+      target_link_libraries(${name} PRIVATE Catch2::Catch2WithMain)
+    else()
+      target_link_libraries(${name} PRIVATE Catch2::Catch2)
+    endif()
+
+    set_target_properties(${name} PROPERTIES RUNTIME_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/bin/tests/unittest")
+    # Register tests.
+    catch_discover_tests(${name})
+
+    # for coverage
+    if (CHAOS_USE_COVERAGE)
+      target_code_coverage(${name} ALL)
+    endif()
+    # TODO. memcheck.
   endif()
 endfunction()
