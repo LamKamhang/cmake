fd53ab45221955dbb5ecd8351cfc4d79535a4e3e
LamKamhang <lamkamh@zju.edu.cn>
2023-08-06 16:05:11 +0800

    add xml

---
+++ packages/pkg_tinyxml2.cmake
@@ -0,0 +1,19 @@
+include_guard()
+
+# if tinyxml2::tinyxml2 has been found
+if (TARGET tinyxml2::tinyxml2)
+  return()
+endif()
+
+message(STATUS "[package/tinyxml2]: tinyxml2::tinyxml2")
+
+if (NOT DEFINED tinyxml2_VERSION)
+  set(tinyxml2_VERSION "9.0.0")
+endif()
+if (NOT DEFINED tinyxml2_TAG)
+  set(tinyxml2_TAG "${tinyxml2_VERSION}")
+endif()
+
+require_package("gh:leethomason/tinyxml2#${tinyxml2_TAG}"
+  CMAKE_ARGS "-Dtinyxml2_BUILD_TESTING=OFF"
+)
