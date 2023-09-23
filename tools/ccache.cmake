e6a77d216136619eb014fd8722b9a21946884485
LamKamhang <lamkamh@zju.edu.cn>
2023-09-23 12:27:24 +0800

    add ccache

---
+++ tools/ccache.cmake
@@ -0,0 +1,15 @@
+include_guard()
+
+# Enable ccache/sccache.
+find_program(CCACHE_FOUND ccache)
+find_program(SCCACHE_FOUND sccache)
+
+if (LAM_USE_SCCACHE AND SCCACHE_FOUND)
+  message(STATUS "[lam/tools]: Enable sccache...")
+  set(CMAKE_CXX_COMPILER_LAUNCHER sccache)
+  set(CMAKE_C_COMPILER_LAUNCHER sccache)
+elseif(LAM_USE_CCACHE AND CCACHE_FOUND)
+  message(STATUS "[lam/tools]: Enable ccache...")
+  set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
+  set(CMAKE_CXX_COMPILER_LAUNCHER ccache)
+endif()
