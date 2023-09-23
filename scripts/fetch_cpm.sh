e44e2fae0a5d1503e5682c131d4dc6d89a9c90a5
LamKamhang <lamkamh@zju.edu.cn>
2023-09-23 17:13:15 +0800

    update cache hash

--- scripts/fetch_cpm.sh
+++ scripts/fetch_cpm.sh
@@ -4,3 +4,4 @@ PROJ_ROOT="$(dirname "$(dirname "$(realpath "$0")")")"
 CPM_VERSION=${CPM_VERSION:-0.38.5}
 echo "fetch CPM.cmake@${CPM_VERSION}"
 wget -O "${PROJ_ROOT}"/core/CPM.cmake "https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_VERSION}/CPM.cmake"
+patch "${PROJ_ROOT}"/core/CPM.cmake < "${PROJ_ROOT}/scripts/cpm_hash.patch"
